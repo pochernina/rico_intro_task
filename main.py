5e11b68fb73a257198cb643d722746687c691ec3
pochernina <olya_pochernina@mail.ru>
2023-02-08 22:32:33 +0300

    add histogram

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from PIL import Image, ImageDraw
 from pathlib import Path
 import argparse
 import json
-from collections import defaultdict
 import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description = 'Rico Intro Task')
@@ -49,11 +48,11 @@ def get_count_of_greyscale_images():
             count += 1
     return count
 
-def draw_bounds(i):
+def get_bounds(i):
     im_path = Path(f'dataset/{i}.jpg')
     json_path = Path(f'dataset/{i}.json')
     if not (im_path.is_file() and json_path.is_file()):
-        return
+        return []
     info_size = (1440, 2560)
     im = Image.open(im_path)
     w, h = im.size
@@ -62,45 +61,42 @@ def draw_bounds(i):
         data = json.load(f)
     data_str = json.dumps(data)
     clickable_elements = data_str.split('"clickable": true')[1:]
-    im_dr = ImageDraw.Draw(im)
+    bounds = []
     for elem in clickable_elements:
         bound = elem.split('"bounds": [')[1].split(']')[0]
         bound = list(map(int, bound.split(',')))
-        print(bound)
         bound[0] = round(bound[0] / info_size[0] * im_size[0])
         bound[1] = round(bound[1] / info_size[1] * im_size[1])
         bound[2] = round(bound[2] / info_size[0] * im_size[0])
         bound[3] = round(bound[3] / info_size[1] * im_size[1])
-        im_dr.rectangle(bound, outline='red', width=8)
+        bounds.append(bound)
+    return bounds
 
+def draw_bounds(i):
+    im_path = Path(f'dataset/{i}.jpg')
+    if not im_path.is_file():
+        return
+    im = Image.open(im_path)
+    im_dr = ImageDraw.Draw(im)
+    bounds = get_bounds(i)
+    for bound in bounds:
+        im_dr.rectangle(bound, outline='red', width=8)
     im.show()
 
 def draw_histogram():
-    dict = defaultdict(int)
-    for i in range(1000):
-        im_path = Path(f'dataset/{i}.jpg')
-        json_path = Path(f'dataset/{i}.json')
-        if not (im_path.is_file() and json_path.is_file()):
-            continue
-        info_size = (1440, 2560)
-        im = Image.open(im_path)
-        w, h = im.size
-        im_size = (w, h) if w < h else (h, w)
-        with open(json_path, 'r') as f:
-            data = json.load(f)
-        data_str = json.dumps(data)
-        clickable_elements = data_str.split('"clickable": true')[1:]
-        for elem in clickable_elements:
-            bound = elem.split('"bounds": [')[1].split(']')[0]
-            bound = list(map(int, bound.split(',')))
-            bound[0] = round(bound[0] / info_size[0] * im_size[0])
-            bound[1] = round(bound[1] / info_size[1] * im_size[1])
-            bound[2] = round(bound[2] / info_size[0] * im_size[0])
-            bound[3] = round(bound[3] / info_size[1] * im_size[1])
-            dict[abs((bound[2] - bound[0]) * (bound[3] - bound[1]))] += 1
-    lst = [key for key, val in dict.items() for _ in range(val)]
-    plt.hist(lst, bins=20)
-    plt.show()
+    # time: 2 min
+    areas = []
+    for i in range(DS_LEN + 1):
+        bounds = get_bounds(i)
+        for bound in bounds:
+            areas.append(abs((bound[2] - bound[0]) * (bound[3] - bound[1])))
+    _, ax = plt.subplots(1, 1)
+    s = 1080 * 1920
+    ax.hist(areas, bins=[0, s // 128, s // 64, s // 32, s // 16, s // 8, s // 4, s // 2,  s])
+    ax.set_title("Areas of clickable elements")
+    ax.set_xlabel('Area size')
+    ax.set_ylabel('Count')
+    plt.savefig('hist.png')
     
 
 if args.task_number == 1:
